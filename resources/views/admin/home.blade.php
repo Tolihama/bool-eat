44fab24c0c0c4ca8c0b6492d797e78af88f0f404
Tolihama <fra070@gmail.com>
2022-02-26 12:52:00 +0100

    Dashboard enhance

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,22 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container py-4">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
+<div class="container py-5">
+    <h1>Dashboard di {{ Auth::user()->name }}</h1>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    <div class="row py-3">
+        <div class="col-sm-12 col-md-6">
+            <h2>I tuoi ordini</h2>
+            <p>Feature da implementare</p>
+        </div>
 
-                    {{ __('You are logged in!') }}
-                </div>
-            </div>
+        <div class="col-sm-12 col-md-6">
+            <h2>Il tuo menù</h2>
+            <p>Feature da implementare</p>
+        </div>
+
+        <div class="col-sm-12 col-md-6">
+            <h2>Il tuo ristorante</h2>
+            <p>Feature da implementare</p>
+        </div>
+
+        <div class="col-sm-12 col-md-6">
+            <h2>Il tuo profilo</h2>
+            <p>Feature da implementare</p>
         </div>
     </div>
 </div>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -8,40 +8,38 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-
             <!-- Right Side Of Navbar -->
             <ul class="navbar-nav ml-auto">
                 <!-- Authentication Links -->
                 @guest
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a class="nav-link" href="{{ route('login') }}">
+                            <i class="fa-solid fa-right-to-bracket mr-2"></i>{{ __('Login') }}
+                        </a>
                     </li>
                     @if (Route::has('register'))
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                            <a class="nav-link" href="{{ route('register') }}">
+                                {{ __('Registrati') }}
+                            </a>
                         </li>
                     @endif
                 @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="{{ route('admin.home') }}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }}
+                    <li class="nav-item mr-2">
+                        <a id="nav-link" class="btn btn-primary" href="{{ route('admin.home') }}">
+                            <i class="fa-solid fa-user icon mr-2"></i> Dashboard di {{ Auth::user()->name }}
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="btn btn-danger" href="{{ route('logout') }}"
+                            onclick="event.preventDefault();
+                                        document.getElementById('logout-form').submit();">
+                            <i class="fa-solid fa-right-from-bracket icon mr-2"></i>{{ __('Logout') }}
                         </a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                               onclick="event.preventDefault();
-                                             document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                @csrf
-                            </form>
-                        </div>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                            @csrf
+                        </form>
                     </li>
                 @endguest
             </ul>
