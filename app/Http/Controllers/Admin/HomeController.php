10b9456d306f222f3f4bfab73e121378927d95f8
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2022-02-28 10:47:03 +0100

    homecontroller fix

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -16,12 +16,14 @@ class HomeController extends Controller
     public function index()
     {
         $user_restaurant = Restaurant::where('user_id', Auth::id())->first();
+
         if ($user_restaurant) {
             $dishes = Dish::where('restaurant_id', $user_restaurant->id)->get();
+            $dishes_paginate = Dish::where('restaurant_id', $user_restaurant->id)->paginate(5);
         } else {
             $dishes = [];
+            $dishes_paginate = $dishes;
         }
-        $dishes_paginate = Dish::where('restaurant_id', $user_restaurant->id)->paginate(5);
         return view('admin.home', compact('user_restaurant', 'dishes', 'dishes_paginate'));
     }
 }
