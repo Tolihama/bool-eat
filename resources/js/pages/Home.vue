2cd941f0cbe40c1c33ec776560d5f8b22106de5f
Tolihama <fra070@gmail.com>
2022-03-02 11:00:47 +0100

    Vue pages and routes setup

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="home" class="container py-5">
+        <Categories />
+        <Restaurants />
+    </div>
+</template>
+
+<script>
+// Components
+import Categories from '../components/Categories';
+import Restaurants from '../components/Restaurants';
+
+export default {
+    name: 'Home',
+    components: {
+        Categories,
+        Restaurants
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
