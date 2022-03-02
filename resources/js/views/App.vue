dcf2fc388b09b51c99ff80264d1dad350a3979a2
Bonu93 <luca.morucci.kb@gmail.com>
2022-03-02 15:44:45 +0100

    add restaurants component

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,21 +1,45 @@
 <template>
     <div id="spa" class="d-flex justify-content-center align-items-center flex-grow-1">
-        <h1>SPA da costruire</h1>
+        
+        <Restaurants :list="restaurants"/>
     </div>
 </template>
 
 <script>
+
+import Restaurants from './components/Restaurants.vue';
+import axios from 'axios';
 export default {
-    name: 'App'
+    name: 'App',
+    components: {
+        Restaurants,
+    },
+    data() {
+        return {
+            restaurants: [],
+        }
+    },
+    created() {
+        this.getRestaurants();
+    },
+    methods: {
+        getRestaurants() {
+            axios.get('http://127.0.0.1:8000/api/restaurants')
+                .then(res => {
+                    this.restaurants = res.data;
+                    console.log(this.restaurants);
+                })
+        }
+    },
 }
 </script>
 
 <style lang="scss">
 #spa {
-    background-image: url('https://www.theromanpost.com/wp-content/uploads/2019/03/MOyarzabal-Terme-Saturnia-Italy.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    filter: brightness(.9);
+    // background-image: url('https://www.theromanpost.com/wp-content/uploads/2019/03/MOyarzabal-Terme-Saturnia-Italy.jpg');
+    // background-repeat: no-repeat;
+    // background-size: cover;
+    // filter: brightness(.9);
 
     h1 {
         font-size: 5rem;
---
+++ resources/js/views/components/Restaurants.vue
@@ -0,0 +1,79 @@
+<template>
+    <div class="container">
+        <div
+            class="row mb-3"
+            v-for="(restaurant, i) in list"
+            :key="`restaurant${i}`"
+        >
+            <div class="col-6">
+                <div class="row">
+                    <div class="col-6">
+                        <div class="restaurant-images">
+                            <div class="cover">
+                                <img
+                                    :src="restaurant.cover"
+                                    :alt="restaurant.name"
+                                />
+                                <div class="thumb">
+                                    <img
+                                        :src="restaurant.thumb"
+                                        :alt="restaurant.thumb"
+                                    />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-6">
+                        <div class="info">
+                            <h5>{{ restaurant.name }}</h5>
+                            <span
+                                class="mx-2"
+                                v-for="(category, i) in restaurant.categories"
+                                :key="`${restaurant.name}-category${i}`"
+                            >
+                                {{ category.name }}
+                            </span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col-6">altre info</div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Restaurants",
+    props: {
+        list: Array,
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+.cover {
+    width: 200px;
+    height: 150px;
+    // border-radius: 20px;
+    // overflow: hidden;
+    position: relative;
+}
+.thumb {
+    width: 100px;
+    height: 75px;
+    // border-radius: 20px;
+    // overflow: hidden;
+    position: absolute;
+    top: 32.5px;
+    right: -50px;
+}
+
+img {
+    height: 100%;
+    width: 100%;
+    border-radius: 20px;
+    object-fit: cover;
+    object-position: center;
+}
+</style>
