40ea5ec72764fb5df60ce9e470a555dcc2cceb4b
francescopalmisanodev <francescopalmisano.dev@gmail.com>
2022-03-02 17:46:08 +0100

    categories component

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,13 +1,13 @@
 <template>
     <div id="home" class="container py-5">
-        <!-- <Categories /> -->
+        <Categories :categoriesList="categories" />
         <Restaurants :list="restaurants"/>
     </div>
 </template>
 
 <script>
 // Components
-// import Categories from '../components/Categories';
+import Categories from '../components/Categories';
 import Restaurants from '../components/Restaurants';
 import axios from 'axios';
 export default {
@@ -15,21 +15,30 @@ export default {
     name: 'Home',
     components: {
         Restaurants,
-        // Categories,
+        Categories,
     },
     data() {
         return {
             restaurants: [],
+            categories: [],
         }
     },
     created() {
         this.getRestaurants();
+        this.getCategories();
     },
     methods: {
         getRestaurants() {
             axios.get('http://127.0.0.1:8000/api/restaurants')
                 .then(res => {
                     this.restaurants = res.data;
+                    // console.log(this.restaurants);
+                })
+        },
+        getCategories() {
+            axios.get('http://127.0.0.1:8000/api/categories')
+                .then(res => {
+                    this.categories= res.data;
                     // console.log(this.restaurants);
                 })
         }
