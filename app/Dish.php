bc63fe107d8145fe3fcf83f351133006c9e85812
Tolihama <fra070@gmail.com>
2022-03-03 11:14:30 +0100

    Add Dishes soft delete

--- app/Dish.php
+++ app/Dish.php
@@ -3,9 +3,15 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Dish extends Model
 {
+    use SoftDeletes;
+
+    /**
+     * MASS ASSIGNMENT
+     */
     protected $fillable = [
         'name',
         'description',
@@ -16,10 +22,17 @@ class Dish extends Model
         'ingredients',
         'is_visible'
     ];
+
+    /**
+     * RELATIONS
+     */
+    // restaurants - dishes (one to many)
     public function restaurant()
     {
         return $this->belongsTo('App\Restaurant');
     }
+
+    // orders - dishes (many to many)
     public function orders()
     {
         return $this->belongsToMany('App\Order');
