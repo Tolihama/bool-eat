f70bfb53b2e4f270ec0b514bb79d73c160da9aee
gabriele-annese <gannese06@gmail.com>
2022-03-05 17:20:21 +0100

    add style paginate

--- resources/js/components/Paginate.vue
+++ resources/js/components/Paginate.vue
@@ -1,53 +1,56 @@
 
 <template>
     <div>
-        <div class="mb-4 ml-3" v-if="paginateData">
-            <!-- first -->
-            <button 
-                class="btn btn-primary" 
-                :disabled="paginateData.currentPage === 1"
-                @click="firstPage" 
-            >
-                <i class="fa-solid fa-angles-left"></i>
-            </button>
+        <div class="d-flex align-items-center justify-content-between mt-4 px-5 py-3">
+            <div class="mb-4" v-if="paginateData">
+                <!-- first -->
+                <button 
+                    class="btn-paginate" 
+                    :disabled="paginateData.currentPage === 1"
+                    @click="firstPage" 
+                >
+                    <i class="fa-solid fa-angles-left"></i>
+                </button>
 
-            <!-- prev -->
-            <button 
-                class="btn btn-primary" 
-                :disabled="paginateData.currentPage === 1"
-                @click="prev"
-            >
-                <i class="fa-solid fa-angle-left"></i>
-            </button>
+                <!-- prev -->
+                <button 
+                    class="btn-paginate" 
+                    :disabled="paginateData.currentPage === 1"
+                    @click="prev"
+                >
+                    <i class="fa-solid fa-angle-left"></i>
+                </button>
 
-            <span class="btn btn-primary">
-                {{paginateData.currentPage}}
-            </span>
+                <span class="paginate-number btn-paginate">
+                    {{paginateData.currentPage}}
+                </span>
 
-            <!-- Next -->
-            <button 
-                class="btn btn-primary" 
+                <!-- Next -->
+                <button 
+                    class="btn-paginate" 
+                    :disabled="paginateData.currentPage === paginateData.lastPage"
+                    @click="next"
+                >
+                    <i class="fa-solid fa-angle-right"></i>
+                </button>
+                
+                <!-- last -->
+                <button 
+                class="btn-paginate" 
+                @click="lastPage"
                 :disabled="paginateData.currentPage === paginateData.lastPage"
-                @click="next"
-            >
-                <i class="fa-solid fa-angle-right"></i>
-            </button>
-            
-            <!-- last -->
-            <button 
-            class="btn btn-primary" 
-            @click="lastPage"
-            :disabled="paginateData.currentPage === paginateData.lastPage"
-            
-            >
-                <i class="fa-solid fa-angles-right"></i>
-            </button>
-
-            <span>
-                {{paginateData.total}} ristoranti aperti
+                >
+                    <i class="fa-solid fa-angles-right"></i>
+                </button>
+            </div>
+            <Loader v-else/>
+            <span class="total-restaurant ">
+                 Ristoranti aperti:
+                <strong class="total-number">
+                    {{paginateData.total}}
+                </strong>
             </span>
         </div>
-        <Loader v-else/>
     </div>
 </template>
 
@@ -112,5 +115,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .btn-paginate{
+        background-color: #f5f3f1;
+        border: 0;
+        border-radius: 50rem;
+        box-shadow: 0 2px 4px -1px rgba(0,0,0,.06),0 1px 10px 1px rgba(0,0,0,.07),0 4px 5px 0 rgba(0,0,0,.03);
+        padding: 18px;
+        top: 50%;
+        margin-right: 10px;
+        cursor: pointer;
+        &:hover {
+            background-color: #f9700b;
+            i{
+                color: #ffff;
+            }
+        }
+
+        i{
+            color: #f9700b;
+            font-size: 20px
+        }
+    }
+    .paginate-number{
+        color: #ec6909;
+        font-weight: 900;
+        cursor: default;
+        &:hover {
+            color: #fff;
+        }
+    }
+    .total-restaurant{
+        font-size: 18px;
+        .total-number{
+            color: #ec6909;
+        }
+    }
 
 </style>
