3c146d35431cb43c65747f58b3c16d1300c24273
Tolihama <fra070@gmail.com>
2022-03-14 15:26:30 +0100

    New Seeder setup

---
+++ config/last-seeder.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    [
+        'name' => 'Zangaloro',
+        'cover' => 'https://tse4.explicit.bing.net/th?id=OIP.-w9fPMZHZ_H-sD6jumQXqAHaFj&pid=Api&P=0&w=219&h=164',
+        'thumb' => 'http://cralunipa.it/media/k2/items/cache/a9ccd7cd1c4267a50c67ac0bd7180172_L.jpg',
+        'address' => 'Via Isidoro la Lumia, 15, 90139 Palermo PA',
+        'vat' => '06245000820',
+        'category' => [3, 9],
+        'dishes' => [
+            [
+                'name' => 'Arancina con carne',
+                'thumb' => 'https://tse4.mm.bing.net/th?id=OIP.qLSiVA5Symt1UvvUXpLwrwHaE6&pid=Api&P=0&w=254&h=168',
+                'ingredients' => 'pastella, riso, ragù, piselli, caciocavallo',
+                'description' => "L'arancina è una specialità della cucina siciliana. Si tratta di una palla o di un cono di riso impanato e fritto  farcito generalmente con ragù",
+                'price' => '1,50',
+            ],
+        ]
+    ],
+];
